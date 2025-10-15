688d498b679dbc8d7ad898ff43686d4bcefb5278
Zer0Programme <iliajafary1388@gmail.com>
2025-10-15 20:57:04 +0330

    first commit

---
+++ project.py
@@ -0,0 +1,69 @@
+print("Hello World !")
+my_list = [1,2,3,4,5]
+number = 6
+my_list.append(number)
+print(my_list)
+ 
+my_list = [1,2,3,4,5]
+number = [6]
+my_list = my_list + [number]
+print(my_list)
+
+my_list = [1,2,3,4,5]
+my_list += [6]
+print(my_list)
+
+#step 4 extend
+my_list = [1,2,3,4,5]
+my_list.extend([6])
+print(my_list)
+
+#step 5 insert
+my_list = [1,2,3,4,5]
+#      0,1,2,3,4
+print(my_list[3])
+
+my_list = [1,2,3,4,5]
+my_list.insert(len(my_list),6)
+print(my_list)
+
+#step six
+#unpacking
+# 3.5 python
+my_list = [1,2,3,4,5]
+my_list = [*my_list,6]
+print(my_list)
+
+#step 7
+#internal method/list add
+#روش داخلی
+my_list = [1,2,3,4,5]
+my_list = my_list.__add__([6])
+print(my_list)
+
+#step 8
+#slicing
+my_list = [1,2,3,4,5]
+my_list[len(my_list):] = [6,7]
+print(my_list)
+
+#step 9
+#operator
+import operator
+my_list = [1,2,3,4,5]
+operator.iadd(my_list,[6,7,8])
+print(my_list)
+
+#step 10
+#chain
+from itertools import chain
+my_list = [1,2,3,4,5]
+my_list = list(chain(my_list, [6,7,8]))
+print(my_list)
+
+#step 11
+#for
+my_list = [1,2,3,4,5]
+for i in [6,7,8]:
+    my_list.append(i)
+    print(my_list)
